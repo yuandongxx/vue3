7d35c62c1fb484649f538e59738cc4be1ef2f68c
yuandongx <786018072@qq.com>
2020-12-14 15:45:00 +0800

    a

--- src/MetaData/MenuItems.js
+++
@@ -1,6 +0,0 @@
-export const Items = [
-    {name: "1", key: "k1", children: null},
-    {name: "2", key: "k2", children: null},
-    {name: "3", key: "k3", children: null},
-    {name: "4", key: "k4", children: null},
-]
